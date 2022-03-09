a3c07f2d130a40236840bbb64b345f656c4a6f22
octavean <octavian.todor04@gmail.com>
2022-03-09 20:52:53 +0200

    added about page, layout changes

--- src/Pages/About/about.js
+++ src/Pages/About/about.js
@@ -1,3 +1,31 @@
 const about = (container) => {
+
+    let box = document.createElement("div");
+    box.setAttribute("id", "about-box")
+
+    let textContent = document.createElement("div");
+    textContent.setAttribute("id", "about-text");
+
+    let h3 = document.createElement("h3");
+    h3.textContent = "About us";
+    textContent.appendChild(h3);
+
+    let p = document.createElement("p");
+    p.textContent = "suntem o corporatie smechera facem bani si suntem smecheri avem pula mare rau din 1980 si futem tot ce prindem! facem sushi bun ca avem talent."
+    textContent.appendChild(p);
+    box.appendChild(textContent);
     
+    let img = document.createElement("img");
+    img.setAttribute("src", "./assets/images/chef-1.jpg");
+    img.setAttribute("id", "about-picture");
+    box.appendChild(img);
+
+    let background = document.createElement("img");
+    background.setAttribute("src", "./assets/images/sushi2.jpg");
+    background.setAttribute("id", "background");
+
+    container.appendChild(background);
+    container.appendChild(box);
 }
+
+export default about;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 import home from "./Pages/Home/home";
-
-const container = document.querySelector("#content");
-
-home(container);
+import about from "./Pages/About/about";
 
 // array with pages and match em up to the p tags on click as well as a function to delete the current node;
 // ez;
 
-const plinks = document.querySelectorAll("p");
-for (let i = 0; i < plinks.length-1; i++) {
-    plinks[i].addEventListener("click", () => {console.log("test")});
+const getContainer = () => {
+    return document.querySelector("#content");
 }
+
+
+const removeNodes = () => {
+    let container = getContainer();
+    for (let i = 0; i < container.childElementCount+1; i++) {
+        if(container.children[0]) {
+            container.children[0].remove();
+        }
+    }
+}
+
+const changePage = (e) => {
+    let container = getContainer();
+    switch(e.target.textContent) {
+        case "Home":
+            home(container)
+            break;
+        case "About":
+            about(container)
+            break;
+    }
+}
+
+const plinks = document.querySelectorAll("p");
+for (let i = 0; i < plinks.length; i++) {
+    plinks[i].addEventListener("click", (e) => {
+        removeNodes();
+        changePage(e)});
+}
+
+
+home(getContainer());
