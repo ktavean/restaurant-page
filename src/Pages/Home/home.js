1e96df978f28b41b595f804227152f0d31dde812
octavean <octavian.todor04@gmail.com>
2022-03-09 17:02:54 +0200

    home page, nav bar, a bit of styling

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,48 +1,17 @@
-function home() {
-    const content = document.querySelector("#content");
-    const ul = document.createElement("ul");
+const home = (container) => {
 
-    const logolist = document.createElement("li");
-    const imglogo = document.createElement("img");
-    imglogo.setAttribute("src", "../dist/assets/images/logo.jpg");
-    imglogo.setAttribute("alt", "logo");
-    logolist.appendChild(imglogo);
-    ul.appendChild(logolist);
+    let box = document.createElement("div");
+    box.setAttribute("id", "home-box")
+    let h2 = document.createElement("h2");
+    h2.textContent = "The best there is..."
+    box.appendChild(h2);
 
-    const homebutlist = document.createElement("li");
-    const homelink = document.createElement("a");
-    homelink.setAttribute("href", "");
-    homelink.textContent = "Home";
-    homebutlist.appendChild(homelink);
-    ul.appendChild(homebutlist);
-
-    const aboutbutlist = document.createElement("li");
-    const aboutlink = document.createElement("a");
-    aboutlink.setAttribute("href", "");
-    aboutlink.textContent = "About";
-    aboutbutlist.appendChild(aboutlink);
-    ul.appendChild(aboutbutlist);
-
-    const menubutlist = document.createElement("li");
-    const menulink = document.createElement("a");
-    menulink.setAttribute("href", "");
-    menulink.textContent = "Menu";
-    menubutlist.appendChild(menulink);
-    ul.appendChild(menubutlist);
-
-    const contactbutlist = document.createElement("li");
-    const contactlink = document.createElement("a");
-    contactlink.setAttribute("href", "");
-    contactlink.textContent = "Contact";
-    contactbutlist.appendChild(contactlink);
-    ul.appendChild(contactbutlist);
-
-    const background = document.createElement("img");
+    let background = document.createElement("img");
     background.setAttribute("id", "background");
     background.setAttribute("src", "../dist/assets/images/sushi.jpg");
 
-    content.appendChild(ul);
-    content.appendChild(background);
+    container.appendChild(background);
+    container.appendChild(box);
 }
 
 export default home;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import home from "./Pages/Home/home";
 
-home();
+const container = document.querySelector("#content");
+
+home(container);
+
+// array with pages and match em up to the p tags on click as well as a function to delete the current node;
+// ez;
+
+const plinks = document.querySelectorAll("p");
+for (let i = 0; i < plinks.length-1; i++) {
+    plinks[i].addEventListener("click", () => {console.log("test")});
+}
